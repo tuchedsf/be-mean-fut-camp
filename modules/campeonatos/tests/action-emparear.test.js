5767505d66c0a58dd54693a73da366475ce5a35a
Diego Ferreira <diego.diegoferreira@gmail.com>
2016-09-01 22:22:01 -0300

    ajuste update password, ajuste geracao jogos, refatoracao

--- modules/campeonatos/tests/action-emparear.test.js
+++ modules/campeonatos/tests/action-emparear.test.js
@@ -36,3 +36,60 @@ describe('Teste Emparear equipes Campeonato', () => {
     });
   });
 });
+
+const timesA2 = ['A', 'B'];
+const timesB2 = ['D', 'C'];
+
+
+describe('Teste Emparear equipes A2 Campeonato', () => {
+  describe('array de 3 times', () => {
+    it('expect return 3 empareamentos', () => {
+      const jogos = emparear(timesA2, timesB2, 1, 1);
+      console.log(jogos);
+      expect(jogos).to.be.instanceof(Array);
+      expect(jogos).to.have.lengthOf(3);
+    });
+    it('expect array index 0  seja A X D', () => {
+      const jogos = emparear(timesA, timesB, 1, 1);
+      expect(jogos).to.be.instanceof(Array);
+      expect(jogos).to.have.lengthOf(3);
+      expect(jogos[0]).to.eql({ mandante: 'A', visitante: 'F', rodada: 1, grupo: 1 });
+    });
+    it('expect array index 1  seja B X C', () => {
+      const jogos = emparear(timesA, timesB, 1, 1);
+      expect(jogos).to.be.instanceof(Array);
+      expect(jogos).to.have.lengthOf(3);
+      expect(jogos[1]).to.eql({ mandante: 'E', visitante: 'B', rodada: 1, grupo: 1 });
+    });
+    it('expect array index 0  seja A X C', () => {
+      const jogos = emparear(timesA, timesB, 1, 1);
+      expect(jogos).to.be.instanceof(Array);
+      expect(jogos).to.have.lengthOf(3);
+      expect(jogos[2]).to.eql({ mandante: 'C', visitante: 'D', rodada: 1, grupo: 1 });
+    });
+     it('expect array index 0  seja D X B', () => {
+      const jogos = emparear(timesA, timesB, 1, 1);
+      expect(jogos).to.be.instanceof(Array);
+      expect(jogos).to.have.lengthOf(3);
+      expect(jogos[2]).to.eql({ mandante: 'C', visitante: 'D', rodada: 1, grupo: 1 });
+    });
+      it('expect array index 0  seja A X B', () => {
+      const jogos = emparear(timesA, timesB, 1, 1);
+      expect(jogos).to.be.instanceof(Array);
+      expect(jogos).to.have.lengthOf(3);
+      expect(jogos[2]).to.eql({ mandante: 'C', visitante: 'D', rodada: 1, grupo: 1 });
+    });
+       it('expect array index 0  seja C X D', () => {
+      const jogos = emparear(timesA, timesB, 1, 1);
+      expect(jogos).to.be.instanceof(Array);
+      expect(jogos).to.have.lengthOf(3);
+      expect(jogos[2]).to.eql({ mandante: 'C', visitante: 'D', rodada: 1, grupo: 1 });
+    });
+    it('expect array index 0  seja C X D', () => {
+      const jogos = emparear(timesA, timesB, 1, 1);
+      expect(jogos).to.be.instanceof(Array);
+      expect(jogos).to.have.lengthOf(3);
+      expect(jogos[2]).to.eql({ mandante: 'C', visitante: 'D', rodada: 1, grupo: 1 });
+    });
+  });
+});
---
+++ modules/campeonatos/tests/action-monta-empareamentos2.test.js
@@ -0,0 +1,110 @@
+'use strict';
+
+const util = require('util');
+const chai = require('chai');
+const expect = chai.expect;
+
+const montaEmpareamentos = require('../actions/action-monta-empareamentos');
+
+const classificacao = [
+  { equipeId: 'A', grupo: 1 },
+  { equipeId: 'B', grupo: 1 },
+  { equipeId: 'C', grupo: 1 },
+  { equipeId: 'D', grupo: 1 },
+];
+
+const timesPorGrupo = 4; // recuperado do campeonato
+const quantidadeGrupos = 1; // recuperado do campeonato
+
+describe('Teste retorno empareamento equipes Campeonato', () => {
+  const empareamentos = montaEmpareamentos(quantidadeGrupos, classificacao, timesPorGrupo, false);
+  console.log(empareamentos);
+  describe('array de 6 empareamentos', () => {
+    it('expect return 6 empareamentos', () => {
+      expect(empareamentos).to.be.instanceof(Array);
+      expect(empareamentos).to.have.lengthOf(6);
+    });
+    it('expect have properties mandante / visitante / rodada / grupo', () => {
+      empareamentos.forEach(element => {
+         expect(element).to.include.keys('mandante');
+         expect(element).to.include.keys('visitante');
+         expect(element).to.include.keys('rodada');
+         expect(element).to.include.keys('grupo');
+      });
+    });
+  });
+  describe('retorno dos empareamentos', () => {
+    it('expect { mandante: "A", visitante: "F", rodada: 1, grupo: 1  } on index 0', () => {
+      expect(empareamentos[0]).to.have.property('mandante', 'A');
+      expect(empareamentos[0]).to.have.property('visitante', 'F');
+      expect(empareamentos[0]).to.have.property('rodada', 1);
+      expect(empareamentos[0]).to.have.property('grupo', 1);
+    });
+    it('expect { mandante: "E", visitante: "B", rodada: 1, grupo: 1  } on index 1', () => {
+      expect(empareamentos[1]).to.have.property('mandante', 'E');
+      expect(empareamentos[1]).to.have.property('visitante', 'B');
+      expect(empareamentos[1]).to.have.property('rodada', 1);
+      expect(empareamentos[1]).to.have.property('grupo', 1);
+    });
+    it('expect { mandante: "C", visitante: "D", rodada: 1, grupo: 1  } on index 2', () => {
+      expect(empareamentos[2]).to.have.property('mandante', 'C');
+      expect(empareamentos[2]).to.have.property('visitante', 'D');
+      expect(empareamentos[2]).to.have.property('rodada', 1);
+      expect(empareamentos[2]).to.have.property('grupo', 1);
+    });
+    it('expect { mandante: "E", visitante: "A", rodada: 2, grupo: 1  } on index 3', () => {
+      expect(empareamentos[3]).to.have.property('mandante', 'E');
+      expect(empareamentos[3]).to.have.property('visitante', 'A');
+      expect(empareamentos[3]).to.have.property('rodada', 2);
+      expect(empareamentos[3]).to.have.property('grupo', 1);
+    });
+    it('expect { mandante: "D", visitante: "F", rodada: 2, grupo: 1  } on index 4', () => {
+      expect(empareamentos[4]).to.have.property('mandante', 'D');
+      expect(empareamentos[4]).to.have.property('visitante', 'F');
+      expect(empareamentos[4]).to.have.property('rodada', 2);
+      expect(empareamentos[4]).to.have.property('grupo', 1);
+    });
+    it('expect { mandante: "B", visitante: "C", rodada: 2, grupo: 1  } on index 5', () => {
+      expect(empareamentos[5]).to.have.property('mandante', 'B');
+      expect(empareamentos[5]).to.have.property('visitante', 'C');
+      expect(empareamentos[5]).to.have.property('rodada', 2);
+      expect(empareamentos[5]).to.have.property('grupo', 1);
+    });
+    it('expect { mandante: "A", visitante: "D", rodada: 3, grupo: 1  } on index 6', () => {
+      expect(empareamentos[6]).to.have.property('mandante', 'A');
+      expect(empareamentos[6]).to.have.property('visitante', 'D');
+      expect(empareamentos[6]).to.have.property('rodada', 3);
+      expect(empareamentos[6]).to.have.property('grupo', 1);
+    });
+    it('expect { mandante: "C", visitante: "E", rodada: 3, grupo: 1  } on index 7', () => {
+      expect(empareamentos[7]).to.have.property('mandante', 'C');
+      expect(empareamentos[7]).to.have.property('visitante', 'E');
+      expect(empareamentos[7]).to.have.property('rodada', 3);
+      expect(empareamentos[7]).to.have.property('grupo', 1);
+    });
+    it('expect { mandante: "F", visitante: "B", rodada: 3, grupo: 1  } on index 8', () => {
+      expect(empareamentos[8]).to.have.property('mandante', 'F');
+      expect(empareamentos[8]).to.have.property('visitante', 'B');
+      expect(empareamentos[8]).to.have.property('rodada', 3);
+      expect(empareamentos[8]).to.have.property('grupo', 1);
+    });
+    it('expect { mandante: "C", visitante: "A", rodada: 4, grupo: 1  } on index 9', () => {
+      expect(empareamentos[9]).to.have.property('mandante', 'C');
+      expect(empareamentos[9]).to.have.property('visitante', 'A');
+      expect(empareamentos[9]).to.have.property('rodada', 4);
+      expect(empareamentos[9]).to.have.property('grupo', 1);
+    });
+    it('expect { mandante: "B", visitante: "D", rodada: 4, grupo: 1  } on index 10', () => {
+      expect(empareamentos[10]).to.have.property('mandante', 'B');
+      expect(empareamentos[10]).to.have.property('visitante', 'D');
+      expect(empareamentos[10]).to.have.property('rodada', 4);
+      expect(empareamentos[10]).to.have.property('grupo', 1);
+    });
+    it('expect { mandante: "E", visitante: "F", rodada: 4, grupo: 1  } on index 11', () => {
+      expect(empareamentos[11]).to.have.property('mandante', 'E');
+      expect(empareamentos[11]).to.have.property('visitante', 'F');
+      expect(empareamentos[11]).to.have.property('rodada', 4);
+      expect(empareamentos[11]).to.have.property('grupo', 1);
+    });
+  });
+});
